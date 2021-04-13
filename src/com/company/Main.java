0ecad984f27a08ada3f0ba248fa80424df5a6ca7
christhao26 <79154880+christhao26@users.noreply.github.com>
2021-04-13 16:36:28 -0400

    java homework

---
+++ src/com/company/Main.java
@@ -0,0 +1,21 @@
+package com.company;
+
+import java.util.Scanner;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner inout = new Scanner(System.in);
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("what is your name?");
+        String firstName = input.nextLine();
+
+
+        System.out.println("Hello " + firstName);
+
+        System.out.println("hello world");
+
+    }
+}
